d26584d378a56332aa81a2792632a7df488b4c39
Constantin ET <emiliantivlica@yahoo.com>
2021-10-20 17:10:17 +0100

    Connect

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_moment import Moment
+import os
 
 app = Flask(__name__)
 # Create a secret key ❖ to create one very easy go to python console log and
 # >>> import secrets
 # >>> secrets.token_hex(ex:16)
-app.config['SECRET_KEY'] = '1acff3c0d6ebf1dd037206a55d48024a'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 # Create database config
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
 # SQLAlchemy error muted
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Create db variable
